88951bb6d135a4bdebb16119a083643263246889
kuroki-ty <t.yamasaki11@gmail.com>
2017-07-25 11:38:31 +0900

    Add XcodeProject

---
+++ MultiThread/MultiThread/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultiThread
+//
+//  Created by tomotaka.yamasaki on 2017/07/25.
+//  Copyright © 2017年 tomotaka.yamasaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
