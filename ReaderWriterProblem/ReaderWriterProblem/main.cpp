aeb0682d2cb8de4342405b4e5f27246ad4d4685d
kuroki-ty <t.yamasaki11@gmail.com>
2017-08-29 17:10:21 +0900

    各クラスの役割を明確にする
    - RWLockクラス: Mutexを使って実際にロックをかけるクラス
    - Dataクラス: Reader,Writerが触るデータ/インスタンスは1つしかない/データを操作するインターフェースを備えている/RWLockを操作するクラス
    - Reader, Writer: ReadとWriteをするだけ/ロックがかかっているかどうかは意識させない

--- ReaderWriterProblem/ReaderWriterProblem/main.cpp
+++ ReaderWriterProblem/ReaderWriterProblem/main.cpp
@@ -13,125 +13,154 @@
 
 #include <unistd.h>
 
+namespace {
+    const int WORD_SIZE = 10;
+    const int RANDOM_WORD_SIZE = 20;
+    const int W_TH_NUM = 3;
+    const int R_TH_NUM = 10;
+}  // namespace
+
 class RWLock
 {
 public:
-    void writeOpen()
+    RWLock()
+    : _readingReaders(0)
+    , _writingWriters(0)
+    , _waitingWriters(0)
     {
-        printf("Write open.\n");
-        _writerNum++;
     }
 
     void readOpen()
     {
-        printf("Read open.\n");
-        _readerNum++;
-    }
+        std::unique_lock<std::mutex> rlock(_mtx);
 
-    void writeClose()
-    {
-        printf("Write close.\n");
-        _writerNum--;
+        // Writerが0 かつ　待ちWriterが0 になるまで待つ
+        _not_writing.wait(rlock, [this] { return _writingWriters <= 0 && _waitingWriters <= 0; });
+
+        _readingReaders++;
+        printf("Read open. Reader:%d\n", _readingReaders);
     }
 
     void readClose()
     {
-        printf("Read close.\n");
-        _readerNum--;
+        std::lock_guard<std::mutex> rlock(_mtx);
+        _readingReaders--;
+        // 待ちスレッドに知らせる
+        _not_writing.notify_all();
+        printf("Read close. Reader:%d\n", _readingReaders);
     }
 
-    void write(const char* word)
+    void writeOpen()
     {
-        // ファイルに書き込む とりあえず文字連結
-        strcat(_file, word);
+        std::unique_lock<std::mutex> wlock(_mtx);
+
+        _waitingWriters++;
+
+        // Readerが0 かつ Writerが0 になるまで待つ
+        _not_writing.wait(wlock, [this]{ return _readingReaders <= 0 && _writingWriters <= 0; });
+
+        _waitingWriters--;
+
+        _writingWriters++;
+        printf("Write open. WaitingWriter:%d\n", _waitingWriters);
     }
 
-    char* read()
+    void writeClose()
     {
-        return _file;
+        std::lock_guard<std::mutex> wlock(_mtx);
+        _writingWriters--;
+        // 待ちスレッドに知らせる
+        _not_writing.notify_all();
+        printf("Write close.\n");
     }
-
-    std::unique_lock<std::mutex> getWLock()
-    {
-        return std::unique_lock<std::mutex>(_wmtx);
-    }
-
-    int getWriterNum() { return _writerNum; }
-    int getReaderNum() { return _readerNum; }
 
 private:
-    // ファイル とりあえず変数にしとく
-    char _file[256];
-
-    int _writerNum = 0;
-    int _readerNum = 0;
-    std::mutex _wmtx;
+    int _readingReaders;
+    int _writingWriters;
+    int _waitingWriters;
+    std::mutex _mtx;
+    std::condition_variable _not_writing;
 };
-
-RWLock rwLock;
-std::condition_variable reading;
-std::condition_variable writing;
 
 void randomSleep()
 {
-    int time = rand() % 3;
+    int time = rand() % 2;
     sleep(time);
 }
 
-// 一定タイミングでファイルを書きに行く人
-void* writer(void *)
+class Data
 {
-    std::unique_lock<std::mutex> wlock = rwLock.getWLock();
-    if (rwLock.getReaderNum() > 0) {
-        reading.wait(wlock, [] { return rwLock.getReaderNum() <= 0; });
-    }
-    rwLock.writeOpen();
-    randomSleep();
-    rwLock.write("x ");
-    rwLock.writeClose();
+public:
+    char* read(char* word)
+    {
+        _rwLock.readOpen();
+        strncpy(word, _buffer, WORD_SIZE);
+        word[WORD_SIZE] = '\n';
+        randomSleep();
+        _rwLock.readClose();
 
-    writing.notify_all();
+        return word;
+    }
+
+    void write(char* word)
+    {
+        _rwLock.writeOpen();
+
+        int offs = (rand() % RANDOM_WORD_SIZE);
+        char* buf2 = word + sizeof(char) * offs;
+        char c = buf2[0];
+        for (int i = 0; i < WORD_SIZE - 1; i++) {
+            _buffer[i] = c;
+            randomSleep();
+        }
+        _rwLock.writeClose();
+    }
+
+private:
+    char _buffer[WORD_SIZE];
+    RWLock _rwLock;
+};
+
+// 一定タイミングでファイルを書きに行く人
+void* writer(void* p)
+{
+    randomSleep();
+
+    Data* data = (Data*)p;
+    char word[] = "ABCDEFGHIJKLMN*+-#,.";
+    data->write(word);
 
     pthread_exit(NULL);
-    return nullptr;
 }
 
 // 一定タイミングでファイルを読みに行く人
-void* reader(void *)
+void* reader(void* p)
 {
-    if (rwLock.getWriterNum() > 0) {
-        std::mutex rmtx;
-        std::unique_lock<std::mutex> rlock(rmtx);
-        writing.wait(rlock);
-    }
-
-    rwLock.readOpen();
     randomSleep();
-    char* word = rwLock.read();
-    printf("R:%s\n", word);
-    rwLock.readClose();
 
-    reading.notify_one();
+    Data* data = (Data*)p;
+    char word[WORD_SIZE];
+    char* ret;
+    ret = data->read(word);
+    printf("read: %s\n", ret);
 
     pthread_exit(NULL);
-    return nullptr;
 }
 
 int main()
 {
-    const int W_TH_NUM = 3;
-    const int R_TH_NUM = 3;
+    Data* data = new Data();
 
     /* スレッドを起動する */
     pthread_t writeThreads[W_TH_NUM];
     for (int i = 0; i < W_TH_NUM; i++) {
-        pthread_create(&writeThreads[i], NULL, writer, NULL);
+        pthread_create(&writeThreads[i], NULL, writer, (void*)data);
     }
 
     /* スレッドを起動する */
     pthread_t readThreads[R_TH_NUM];
     for (int i = 0; i < R_TH_NUM; i++) {
-        pthread_create(&readThreads[i], NULL, reader, NULL);
+        pthread_create(&readThreads[i], NULL, reader, (void*)data);
     }
 
     /* スレッドの終了を待つ */
