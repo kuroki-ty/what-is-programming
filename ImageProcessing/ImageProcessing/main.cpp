5a6f70e935c1bd0717921dceb3ef899b303673aa
kuroki-ty <t.yamasaki11@gmail.com>
2017-10-24 17:50:25 +0900

    Create Project

---
+++ ImageProcessing/ImageProcessing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageProcessing
+//
+//  Created by tomotaka.yamasaki on 2017/10/24.
+//  Copyright © 2017年 tomotaka.yamasaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
