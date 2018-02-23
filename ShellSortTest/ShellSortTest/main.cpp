7b8e34bb92ba76cb82bcdc62b6b54943dd69850c
kuroki-ty <t.yamasaki11@gmail.com>
2018-02-23 20:01:31 +0900

    feat: ShellSortTestプロジェクトを追加

---
+++ ShellSortTest/ShellSortTest/main.cpp
@@ -0,0 +1,60 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// 引数をソートして返す
+// a ソートする配列
+// size 配列のサイズ
+void shell_sort(int a[], int size)
+{
+    int i, j;
+    int h = 1;
+
+    do {
+        h = h * 2 + 1;
+    } while (h <= size);
+
+    do {
+        h /= 3;
+        for (i = h; i < size; i++)
+        {
+            int v = a[i];
+            for (j = i; j > h && a[j - h] > v; j -= h)
+                a[j] = a[j - h];
+            if (i != j)
+                a[j] = v;
+        }
+    } while (h != 1);
+}
+
+
+// 引数となる文字列をソートして標準出力へprintする
+// argc 文字列の個数
+// argv 文字列へのポインタ配列
+int sort_print(int argc, char *argv[])
+{
+    int *a;
+    int i;
+
+    a = (int *)malloc((argc - 1) * sizeof(int));
+    for (i = 0; i < argc - 1; i++)
+        a[i] = atoi(argv[i + 1]);
+
+    shell_sort(a, argc);
+
+    printf("Output : ");
+    for (i = 0; i < argc - 1; i++)
+        printf("%d ", a[i]);
+
+    printf("\n");
+
+    free(a);
+
+    return 0;
+}
+
+// コマンドライン引数をソートしてprintする
+int main(int argc, char *argv[])
+{
+    return sort_print(argc, argv);
+}
